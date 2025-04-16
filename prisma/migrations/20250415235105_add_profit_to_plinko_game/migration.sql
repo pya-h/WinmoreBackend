bacf4ad8fc5d166c069df0f2af53619e4c602892
pya-h <p4ya.py@gmail.com>
2025-04-16 12:24:58 +0330

    revise migrations

--- prisma/migrations/20250415235105_add_profit_to_plinko_game/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "PlinkoGame" ADD COLUMN     "profit" DOUBLE PRECISION;
--- prisma/migrations/20250416084743_add_plinko_and_referral_tables/migration.sql
+++ prisma/migrations/20250416084743_add_plinko_and_referral_tables/migration.sql
@@ -1,5 +1,15 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[referralCode]` on the table `UserProfile` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `referralCode` to the `UserProfile` table without a default value. This is not possible if the table is not empty.
+
+*/
 -- CreateEnum
 CREATE TYPE "PlinkoGameStatus" AS ENUM ('NOT_DROPPED_YET', 'DROPPING', 'FINISHED');
+
+-- AlterTable
+ALTER TABLE "UserProfile" ADD COLUMN     "referralCode" TEXT NOT NULL;
 
 -- CreateTable
 CREATE TABLE "PlinkoGame" (
@@ -14,6 +24,7 @@ CREATE TABLE "PlinkoGame" (
     "mode" "GameModesEnum" NOT NULL DEFAULT 'EASY',
     "rowsCount" INTEGER NOT NULL DEFAULT 8,
     "prize" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "profit" DOUBLE PRECISION,
     "status" "PlinkoGameStatus" NOT NULL DEFAULT 'NOT_DROPPED_YET',
     "finishedAt" TIMESTAMP(3),
 
@@ -22,7 +33,7 @@ CREATE TABLE "PlinkoGame" (
 
 -- CreateTable
 CREATE TABLE "PlinkoBalls" (
-    "id" SERIAL NOT NULL,
+    "id" BIGSERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "gameId" INTEGER NOT NULL,
@@ -54,8 +65,21 @@ CREATE TABLE "PlinkoRules" (
     CONSTRAINT "PlinkoRules_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Referral" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "referrerId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "layer" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Referral_pkey" PRIMARY KEY ("referrerId","userId")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "PlinkoRules_rows_key" ON "PlinkoRules"("rows");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserProfile_referralCode_key" ON "UserProfile"("referralCode");
 
 -- AddForeignKey
 ALTER TABLE "PlinkoGame" ADD CONSTRAINT "PlinkoGame_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -68,3 +92,9 @@ ALTER TABLE "PlinkoBalls" ADD CONSTRAINT "PlinkoBalls_gameId_fkey" FOREIGN KEY (
 
 -- AddForeignKey
 ALTER TABLE "PlinkoBalls" ADD CONSTRAINT "PlinkoBalls_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Referral" ADD CONSTRAINT "Referral_referrerId_fkey" FOREIGN KEY ("referrerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Referral" ADD CONSTRAINT "Referral_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
