a3d371cb6c9bdd7f2aaed0580fbc52a2d3eb396f
pya-h <p4ya.py@gmail.com>
2025-06-12 09:27:01 +0330

    add disconnections sequence to prisma for discretion

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,19 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   onModuleInit() {
     this.$connect()
       .then(() => console.log('Connected to database successfully.'))
       .catch((err) =>
         console.error('Database connection fucked up, since: ', err),
       );
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
